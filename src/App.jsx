646c5871177f6b06496b8e668970818930b84cc0
Caio Guimarães <profisional.caioguimaraes@gmail.com>
2024-06-03 16:41:08 -0300

    installing CORS and AXIOS and create API connection file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-/* import { SignUp } from "./pages/SignUp" */
+import { SignUp } from "./pages/SignUp"
 /* import { Home } from "./pages/Home" */
 /* import { Dish } from "./pages/Dish" */
 /* import { AddDish } from "./pages/AddDish" */
-import { EditDish } from "./pages/EditDish"
+/* import { EditDish } from "./pages/EditDish" */
 
 function App() {
-  return <EditDish />
+  return <SignUp />
 }
 
 export default App
