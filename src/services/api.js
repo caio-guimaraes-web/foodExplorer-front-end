b5b27165567299510451ac37e86f0ddeb0f38650
Caio Guimarães <profisional.caioguimaraes@gmail.com>
2024-08-08 17:52:57 -0300

    connect to prod back-end

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,19 @@
 import axios from "axios"
 
-export const api = axios.create({
+/* dev enviromment - eu sei que isso tá feio, em breve deixo isso melhor */
+/* export const api = axios.create({
   baseURL: "http://localhost:3333",
 })
 
 export const getCardImageUrl = () => {
   return "http://localhost:3333/files/"
+} */
+
+/* production enviromment */
+export const api = axios.create({
+  baseURL: "https://foodexplorer-back-end-gaba.onrender.com",
+})
+
+export const getCardImageUrl = () => {
+  return "https://foodexplorer-back-end-gaba.onrender.com/files/"
 }
